16fd3f5ef9009905171edad3a9ea20078d528dc2
aaron <aaron777collins@gmail.com>
2022-06-23 15:59:22 -0400

    Added typescript and a proper test for our server

--- site/backend/app.js
+++ site/backend/app.js
@@ -6,6 +6,11 @@ app.post("/post", (req, res) => {
     res.redirect("/");
 });
 
+app.post("/api", (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ resp: "test" }));
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
